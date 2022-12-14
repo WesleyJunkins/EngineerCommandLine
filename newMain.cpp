86792d91f813c5c3100db1a6f5951f8c35a1f6aa
WesleyJunkins <67446010+WesleyJunkins@users.noreply.github.com>
2022-12-13 21:23:25 -0600

    Made the command line input work properly

--- newMain.cpp
+++ newMain.cpp
@@ -8,37 +8,85 @@ using namespace std;
 int main()
 {
     string commandLine;
+    string token;
     enum mode {main, add, sub};
     mode currentMode = main;
 
-    while(1)
+    while(currentMode == main)
     {
-        cout<<endl;
-        vector<string> command;
-        if(currentMode == main)
-        {
-            cout<<"main> ";
-        }
-        while((cin>>commandLine))
-        {
-            if(commandLine == "exit")
-            {
-                return 0;
-            }
-            if(commandLine == "go")
-            {
-                break;
-            }
-            command.push_back(commandLine);
-        }
+        cout<<endl<<endl<<"main> ";
+        vector <string> command;
+        getline(cin, commandLine); // use getline() function to read a line of string and store into commandLine variable.  
+        stringstream inputStream(commandLine); // inputStream is an object of stringstream that references the commandLine string  
+        while (getline(inputStream, token, ' '))   // use while loop to check the getline() function condition  
+        {  
+            /* inputStream is used to read the string from stringstream, 
+            token is used to store the token string (one word between each whitespace), and 
+            ' ' whitespace is used to split the string where whitespace is found. 
+            */  
+            command.push_back(token);  //add each token to the command vector that will be used to identify each command.
 
-        for(int i = 0; i < command.size(); i++)
+        }  
+
+        //Start reading each command token here and call the corresponding function. (Should not have to put endl inside or after calling any function since there is a cout with two endl's at the beginning of the main while loop)
+        if(command.front() == "exit")
         {
-            cout<<command.at(i)<<" ";
+            return 0;
         }
+        if(command.front() == "wes")
+        {
+            command.erase(command.begin());
+            if(command.front() == "is")
+            {
+                command.erase(command.begin());
+                if(command.front() == "cool")
+                {
+                    cout<<"You Better Believe it!"<<endl;
+                    return 0;
+                }
+                
+            }
+            
+        }
+        else
+        {
+            cout<<"Command not recognized. Type \"helo\" for a description of all commands.";
+        }
+        command.clear();
     }
-    
-
-
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//DO NOT DELETE: THIS IS A TEMPLATE
+// if(command.front() == "wes")
+// {
+//     command.erase(command.begin());
+    
+// }
